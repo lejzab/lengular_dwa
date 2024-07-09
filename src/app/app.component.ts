68833bcc4b2c4576cd3de78b9228fddee27127eb
Paweł Bogaczewicz <0x645@tutanota.com>
2024-07-09 19:47:14 +0200

    usunięcie niepotrzebnych importów

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import type {
-  InvestmentInput,
-  InvestmentResult,
-} from './investment-input.model';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
@@ -13,6 +9,4 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
   templateUrl: './app.component.html',
   imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
-export class AppComponent {
-  investmentResults = signal<InvestmentResult[]>([]);
-}
+export class AppComponent {}
